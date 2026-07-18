2c8a64181c5d64e3e43697674d33a2c24635aef3
jatintaliyan <jatintaliyan91@gmail.com>
2026-07-18 15:34:00 +0530

    Create Header.jsx

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './header.module.css'
+
+function Header(){
+  return (
+    <>
+    <div className={styles.header}>
+        <h1 className={styles._heading}>MIET</h1>
+        <h2 className={styles._heading}>CSE(IOT) STUDENTS</h2>
+    </div>
+
+    
+    <div className={styles.unheader}>
+        <div className={styles._rankbtn}>
+                <a href="#_graph" className={styles.seer}>
+                    SEE RANKING
+                </a>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
