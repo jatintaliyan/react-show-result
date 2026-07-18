58684c832300a935a91a29936af3ed6198abb382
jatintaliyan <jatintaliyan91@gmail.com>
2026-07-18 15:41:12 +0530

    Create Footer.jsx

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+function Footer(){
+  return (
+    <>
+    <div className={styles._footer}>
+        <h6 className={styles._author}> BY JATIN TALIYAN</h6>
+    </div>
+    </>
+  )
+}
+
+export default Footer
